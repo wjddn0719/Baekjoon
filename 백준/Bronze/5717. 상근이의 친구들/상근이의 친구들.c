c4219a0424c50601bcc8813d47ebcd466d4023e3
이정우 <jungwoo07191@naver.com>
2025-05-21 13:20:59 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int M,F;
+    while (1){
+        scanf("%d %d", &M, &F);
+        if(M!=0 && F!=0){
+            printf("%d\n", M+F);
+        }
+        else break;
+    }
+    return 0;
+}
