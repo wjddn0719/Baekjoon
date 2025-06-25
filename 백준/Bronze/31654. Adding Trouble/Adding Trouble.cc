3f26834e064dff4d45fface221e596d8a76f193a
wjddnlee <jungwoo07191@naver.com>
2025-06-25 20:06:28 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a+b==c)cout<<"correct!";
+    else cout<<"wrong!";
+}
