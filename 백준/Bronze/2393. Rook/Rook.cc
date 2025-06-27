49a2363f33735f62af3c41249955591b71a8733b
wjddnlee <jungwoo07191@naver.com>
2025-06-27 16:58:11 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\\n";
+    return 0;
+}
