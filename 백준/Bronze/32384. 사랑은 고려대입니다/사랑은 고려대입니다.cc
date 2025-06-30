2f9f355a1ac4e43b6e09b7edca6e6848eae5af47
wjddnlee <jungwoo07191@naver.com>
2025-07-01 07:34:18 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a;
+    cin >> a;
+    for(int i=1; i<=a; i++){
+        cout << "LoveisKoreaUniversity"<<" ";
+    } 
+    return 0;
+}
