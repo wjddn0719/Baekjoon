ce2038a6809214b468a16a7fe2f54af71cf25879
wjddnlee <jungwoo07191@naver.com>
2025-07-21 13:21:23 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    for(int i=a; i>=1; i--){
+        for(int j=1; j<=a-i; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=i*2-1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
