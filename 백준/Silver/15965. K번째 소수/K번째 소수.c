78740bc5835eae9bdc81a66db50e2eb19462d6fb
wjddnlee <jungwoo07191@naver.com>
2025-08-08 22:09:23 +0900

    [Silver II] Title: K번째 소수, Time: 856 ms, Memory: 79236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.c
+++ 백준/Silver/15965. K번째 소수/K번째 소수.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int map[500001]={0,}, is_prime[500001]={0,};
+int map[10000001]={0,}, is_prime[10000001]={0,};
 
 int main(){
 	int N;
-	for(int i=2; i*i<=500000; i++){
-		for(int j=i*i; j<=500000; j+=i){
+	for(int i=2; i*i<=10000000; i++){
+		for(int j=i*i; j<=10000000; j+=i){
 			map[j] = 1;
 		}
 	}
 	int k =1;
-	for(int i=2; i<=500000; i++){
+	for(int i=2; i<=10000000; i++){
 		if(map[i]==0){
 			is_prime[k] = i;
 			k++;
